c816254856e200df6388b5d6ac1dc9c546e40427
Harish0024 <145547051+Harish0024@users.noreply.github.com>
2023-10-30 10:30:48 +0530

    Add files via upload

---
+++ tagglifecycle/MyComponents/User.js
@@ -0,0 +1,17 @@
+
+
+class user  {
+
+
+    constructor(firstName,lastName,Address){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        
+        this.Address=Address;
+    }
+    firstName='Unknown';
+    lastName='unknown';
+   
+    Address='unknown';
+}
+export default user;
