ce4dd2fa03bd5a202023c34ff9fba695af143cd5
Harish0024 <145547051+Harish0024@users.noreply.github.com>
2023-10-30 10:17:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+//it is a build in function which is used to define a state in function
 
 function App() {
+ const [firstName, setFirstName] = useState('Unknown');
+  const [lastName, setLastName] = useState('Unknown');
+  const [agree, setAgree] = useState(false);
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+
+ 
+
+  const updateFirstName = (event) => {
+    const value = event.target.value;
+    setFirstName(value);
+    if (value.length < 5) {
+      setFirstNameError('First name must have at least 5 characters.');
+    } else {
+      setFirstNameError('');
+    }
+  }
+
+ 
+
+  const updateLastName = (event) => {
+    const value = event.target.value;
+    setLastName(value);
+    if (value.length < 5) {
+      setLastNameError('Last name must have at least 5 characters.');
+    } else {
+      setLastNameError('');
+    }
+  }
+
+ 
+
+  const enableDisable = (event) => {
+    setAgree(event.target.checked);
+  }
+
+ 
+
+  const isSaveButtonEnabled = firstName.length >= 5 && lastName.length >= 5 && agree;
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <b> Welcome to React JS.</b>
-          In react JS Single page application
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<form>
+        Enter your First name <input type="text" value={firstName} onChange={updateFirstName} /><br />
+        {firstNameError && <div className="error">{firstNameError}</div>}
+        Enter your Last name <input type="text" value={lastName} onChange={updateLastName} /><br />
+        {lastNameError && <div className="error">{lastNameError}</div>}
+<b>Your Name</b> {firstName}&nbsp;&nbsp;{lastName}<br />
+<input type="checkbox" value={agree} onChange={enableDisable} /> Agree to Terms & Conditions<br />
+<input type="button" value="Save" disabled={!isSaveButtonEnabled} />
+</form>
+</div>
   );
 }
 
